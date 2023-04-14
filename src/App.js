41f98981da37c3df8d6f3dec3552f0e254257dad
Paloma Wink <paloma.wink@hotmail.com>
2023-04-13 21:14:05 -0300

    Requisito 2 concluido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -14,14 +14,14 @@ class App extends React.Component {
       <div>
         <p>TrybeTunes!</p>
         <NotFound />
-        <switch>
-          <Route path="/" component={ Login } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
-        </switch>
+          <Route path="/profile" component={ Profile } />
+        </Switch>
       </div>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  state = {
+    buttonSubmit: true,
+    nameField: '',
+    loading: false,
+  };
+
+  handleClick = async () => {
+    const { nameField } = this.state;
+    const { history } = this.props;
+    // loading precisa ser true
+    this.setState({
+      loading: true,
+    });
+
+    await createUser({
+      name: nameField,
+    });
+    // Preciso redirecionar para o search
+    history.push('/search');
+  };
+
+  handleButtonChange = ({ target: { value } }) => {
+    if (value.length >= '3') {
+      this.setState({
+        buttonSubmit: false,
+      });
+    }
+    this.setState({
+      nameField: value,
+    });
+  };
+
   render() {
+    const { nameField, buttonSubmit, loading } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div>
+        <div data-testid="page-login">Login</div>
+        {loading ? <Loading />
+          : (
+            <div>
+              <input
+                type="text"
+                data-testid="login-name-input"
+                placeholder="Escreva seu nome"
+                value={ nameField }
+                onChange={ this.handleButtonChange }
+              />
+              <button
+                data-testid="login-submit-button"
+                disabled={ buttonSubmit }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+            </div>
+          )}
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
