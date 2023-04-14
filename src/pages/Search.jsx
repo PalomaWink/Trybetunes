17d48647a58a11e337faced05c943ddd0c9faa4f
Paloma Wink <paloma.wink@hotmail.com>
2023-04-14 18:05:04 -0300

    Requisito 5 concluido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    buttonSubmit: true,
+    searchField: '',
+  };
+
+  handleClick = ({ target: { value } }) => {
+    if (value.length >= '2') {
+      this.setState({
+        buttonSubmit: false,
+      });
+    }
+    this.setState({
+      searchField: value,
+    });
+  };
+
   render() {
+    const { searchField, buttonSubmit } = this.state;
     return (
-      <div data-testid="page-search">Search</div>
+      <div data-testid="page-search">
+        <span>Search</span>
+        <input
+          data-testid="search-artist-input"
+          placeholder="Nome do Artista"
+          value={ searchField }
+          onChange={ this.handleClick }
+        />
+        <button
+          data-testid="search-artist-button"
+          disabled={ buttonSubmit }
+          // onClick={}
+        >
+          Pesquisar
+        </button>
+      </div>
     );
   }
 }
